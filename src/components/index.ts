71ddb062517b2e8fb41c15e3a4bfd043683e5749
Abkeyev Daniyar <abkeyev@gmail.com>
2020-01-30 17:40:06 +0600

    remove unnecessary files

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Main } from './Main';
 export { default as MobileBanking } from './MobileBanking';
-export { default as AdditionalInfo } from './AdditionalInfo/AdditionalInfo';
 export { default as BccInputText } from './BccInputText';
 export { default as BccSwitch } from './BccSwitch';
 export { default as Loading } from './Loading';
